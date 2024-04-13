24fc0e676f659c8d49812fa6f7dd9e03e2add340
Evgeniy <Evgeniy@DESKTOP-8MJVOQS>
2024-04-13 15:07:23 +0300

    Add project files.

---
+++ lab 13/lab 13.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream> 
+using namespace std;
+double computeSeries(int n) {
+	if (n == 1)
+		return 20.0;
+	else
+		return computeSeries(n - 1) / 2.0;
+}
+
+int main() {
+	int n;
+	cout << "Input n: ";
+	cin >> n;
+
+	if (n < 0) {
+		cout << "N must be bigger than 0" << endl;
+		return 1;
+	}
+
+	double result = computeSeries(n);
+	cout << "Result: " << result << endl;
+
+	return 0;
+}
